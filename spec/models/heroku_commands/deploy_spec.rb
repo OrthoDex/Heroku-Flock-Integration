d83833acc8468d4e8483dfea61dd5c6e9bc96e51
Corey Donohoe <atmos@atmos.org>
2016-03-17 05:16:08 -0700

    start adding heroku deploy with pipelines

---
+++ spec/models/heroku_commands/deploy_spec.rb
@@ -0,0 +1,54 @@
+require "rails_helper"
+
+RSpec.describe HerokuCommands::Deploy, type: :model do
+  include SlashHeroku::Support::Helpers::Api
+
+  before do
+  end
+
+  def heroku_handler_for(text)
+    command = command_for(text)
+    command.handler
+  end
+
+  # rubocop:disable Metrics/LineLength
+  it "has a deploy:info command" do
+    command = heroku_handler_for("deploy:info -a hubot")
+
+    response_info = fixture_data("account/info")
+    stub_request(:get, "https://api.heroku.com/account")
+      .with(headers: default_headers(command.user.heroku_token))
+      .to_return(status: 200, body: response_info, headers: {})
+
+    response_info = fixture_data("pipelines/info")
+    stub_request(:get, "https://api.heroku.com/pipelines")
+      .with(headers: default_headers(command.user.heroku_token))
+      .to_return(status: 200, body: response_info, headers: {})
+
+    response_info = fixture_data("pipelines/531a6f90-bd76-4f5c-811f-acc8a9f4c111/pipeline-couplings")
+    stub_request(:get, "https://api.heroku.com/pipelines/531a6f90-bd76-4f5c-811f-acc8a9f4c111/pipeline-couplings")
+      .with(headers: default_headers(command.user.heroku_token))
+      .to_return(status: 200, body: response_info, headers: {})
+
+    response_info = fixture_data("apps/27bde4b5-b431-4117-9302-e533b887faaa")
+    stub_request(:get, "https://api.heroku.com/apps/27bde4b5-b431-4117-9302-e533b887faaa")
+      .with(headers: default_headers(command.user.heroku_token))
+      .to_return(status: 200, body: response_info, headers: {})
+
+    expect(command.task).to eql("deploy")
+    expect(command.subtask).to eql("info")
+    expect(command.application).to eql("hubot")
+    expect { command.run }.to_not raise_error
+    expect(command.response[:response_type]).to eql("in_channel")
+    expect(command.response[:attachments].size).to eql(1)
+    attachment = command.response[:attachments].first
+    expect(attachment[:fallback])
+      .to eql("Heroku deploy for hubot")
+    expect(attachment[:pretext]).to eql(nil)
+    expect(attachment[:text].split("\n").size).to eql(1)
+    expect(attachment[:title])
+      .to eql("<https://dashboard.heroku.com/pipelines/531a6f90-bd76-4f5c-811f-acc8a9f4c111|hubot>")
+    expect(attachment[:title_link]).to eql(nil)
+    expect(attachment[:fields]).to eql(nil)
+  end
+end
