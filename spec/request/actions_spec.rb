1a7655632b5f2dd018b30a07b8933f9a13c23f78
Yannick Schutz <yannick.schutz@gmail.com>
2016-11-29 14:45:53 -0800

    Fix the rubocop's braces

--- spec/request/actions_spec.rb
+++ spec/request/actions_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "SlashHeroku /actions", type: :request do
   it "404s if the incoming action isn't from slack" do
-    post "/actions", params: { payload: JSON.dump({ token: "rando-token" }) }
+    post "/actions", params: { payload: JSON.dump(token: "rando-token") }
 
     expect(status).to eql(404)
     response_body = JSON.parse(body)
@@ -10,7 +10,7 @@ RSpec.describe "SlashHeroku /actions", type: :request do
   end
 
   it "204s the incoming action is from slack" do
-    post "/actions", params: { payload: JSON.dump({ token: "secret-slack-token" }) }
+    post "/actions", params: { payload: JSON.dump(token: "secret-slack-token") }
 
     expect(status).to eql(204)
   end
