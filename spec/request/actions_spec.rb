3854f4012d6e251bc0050fd2ce82e81b37a9a4c8
Yannick Schutz <yannick.schutz@gmail.com>
2016-11-29 13:58:02 -0800

    Add basic actions controller action

---
+++ spec/request/actions_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe "SlashHeroku /actions", type: :request do
+  it "404s if the incoming action isn't from slack" do
+    post "/actions", params: { payload: JSON.dump({ token: "rando-token" }) }
+
+    expect(status).to eql(404)
+    response_body = JSON.parse(body)
+    expect(response_body).to eql({})
+  end
+
+  it "200s if the incoming action is from slack" do
+    post "/actions", params: { payload: JSON.dump({ token: "secret-slack-token" }) }
+
+    expect(status).to eql(200)
+    response_body = JSON.parse(body)
+    expect(response_body).to eql({})
+  end
+end
