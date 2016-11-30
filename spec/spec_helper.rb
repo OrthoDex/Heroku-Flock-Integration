de62e3c2c6dd27aecfc3de34e039f7e07cce56bb
Yannick Schutz <yannick.schutz@gmail.com>
2016-11-29 16:43:43 -0800

    Extract .env.test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,3 @@
-ENV["APP_NAME"] = "slash-heroku"
-ENV["HOSTNAME"] = "www.example.com"
-ENV["SLACK_APP_URL"] = "https://slack.com/apps/manage/A0SFS6WSD-heroku"
-ENV["KOLKRABBI_HOSTNAME"] = "kolkrabbi.com"
-ENV["PRIVACY_POLICY_URL"] = "https://www.example.com/privacy.html"
-ENV["SLACK_SLASH_COMMAND_TOKEN"] = "secret-slack-token"
-ENV["FERNET_SECRET"] = "2bb0Wq1HJDPg2eLRnvTbKh8xPtJzqJnonOi3fDyVIJ8="
-ENV["GITHUB_ADMIN_LOGINS"] = "atmos"
-
 require "webmock/rspec"
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
