fb343db61c2daa545fa379b25082d6ffd7d6a33f
Yannick Schutz <yannick.schutz@gmail.com>
2016-11-29 19:56:29 -0800

    Yo method length

--- spec/request/message_actions_spec.rb
+++ spec/request/message_actions_spec.rb
@@ -2,7 +2,8 @@ require "rails_helper"
 
 RSpec.describe "SlashHeroku /message_actions", type: :request do
   it "404s if the incoming action isn't from slack" do
-    post "/message_actions", params: { payload: JSON.dump(token: "rando-token") }
+    post "/message_actions",
+      params: { payload: JSON.dump(token: "rando-token") }
 
     expect(status).to eql(404)
     response_body = JSON.parse(body)
@@ -10,7 +11,8 @@ RSpec.describe "SlashHeroku /message_actions", type: :request do
   end
 
   it "204s the incoming action is from slack" do
-    post "/message_actions", params: { payload: JSON.dump(token: "secret-slack-token") }
+    post "/message_actions",
+      params: { payload: JSON.dump(token: "secret-slack-token") }
 
     expect(status).to eql(204)
   end
