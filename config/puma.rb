f53ef0d307b1ee1de795dd6d3fbfc0b441e2c928
Ishaan Malhi <ishaan.malhi@gmail.com>
2017-02-19 01:15:38 +0530

    fix: change puma port

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
-port        3000
+port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 on_worker_boot do
